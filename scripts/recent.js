8bff82894abbf464e2b52faa38a9a080329b98b1
joohei <joona.heimonen@icloud.com>
2022-02-17 20:23:58 +0200

    j

--- scripts/recent.js
+++ scripts/recent.js
@@ -1,16 +1,18 @@
 const accessToken = "IGQVJWWnRyVXhkNlVWckk2Rkd2dThNeW5VQUhNZAGhLNTFEcWl1c0JCMFBSRTk3X0FVSDY4RFljZAjRZAVnRrYU5aV2pEV2lIblZA4RWlxQUg3enZAWS2JOYXcxZAmRZAbmFSdjVENktXaldaSHRHSGhLYkh4VAZDZD";
 
-let dataUrl = `https://graph.instagram.com/me/media?fields=media_url,caption&access_token=${accessToken}`;
+let dataUrl = `https://graph.instagram.com/me/media?fields=media_url,caption, timestamp&access_token=${accessToken}`;
 
 async function getMedia(dataUrl) {
   let response = await fetch(dataUrl);
   let data = await response.json();
   let img = document.createElement("img");
+  let date = document.createTextNode(data.data[0].timestamp)
   let imgUrl = data.data[0].media_url
   img.src = imgUrl;
   img.alt = data.data[0].caption;
   let recent = document.getElementsByClassName("recent")[0];
   recent.appendChild(img);
+  recent.appendChild(date);
 }
 
 getMedia(dataUrl);
