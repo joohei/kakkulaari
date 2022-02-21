d9b41e4b91937dc09e5be8f5f9016ae995f6b7e9
joohei <joona.heimonen@icloud.com>
2022-02-21 22:34:41 +0200

    Instagram API changes

--- scripts/recent.js
+++ scripts/recent.js
@@ -1,11 +1,15 @@
 const accessToken = "IGQVJWWnRyVXhkNlVWckk2Rkd2dThNeW5VQUhNZAGhLNTFEcWl1c0JCMFBSRTk3X0FVSDY4RFljZAjRZAVnRrYU5aV2pEV2lIblZA4RWlxQUg3enZAWS2JOYXcxZAmRZAbmFSdjVENktXaldaSHRHSGhLYkh4VAZDZD";
 
-let dataUrl = `https://graph.instagram.com/me/media?fields=media_url,caption, timestamp&access_token=${accessToken}`;
+const dataUrl = `https://graph.instagram.com/me/media?fields=media_url,caption, timestamp&access_token=${accessToken}`;
+
+const howMany = 5
 
 async function getMedia(dataUrl) {
-  let response = await fetch(dataUrl);
-  let data = await response.json();
-  for (let i = 0; i < 5; i++) {
+  let data = await fetch('API.json')
+    .then(function (response) {
+      return response.json();
+    })
+  for (let i = 0; i < howMany; i++) {
     let header = document.createElement("h5")
     let date = data.data[i].timestamp.substring(0, 10);
     date = date.split('-');
