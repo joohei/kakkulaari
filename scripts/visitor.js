f92a34240d6c6567afc1412f913e89ee3d49feb5
joohei <joona.heimonen@icloud.com>
2022-02-22 12:51:22 +0200

    Add files via upload

--- scripts/visitor.js
+++ scripts/visitor.js
@@ -1,12 +1,11 @@
-const hitUrl = "https://api.countapi.xyz/get/kakkulaari/mollamersu";
+const hitUrl = "https://api.countapi.xyz/hit/kakkulaari/mollamersu";
 const visitorCounter = document.querySelector(".visitor-counter")
 
 async function newVisitor(hitUrl, visitorCounter) {
   let response = await fetch(hitUrl);
   let data = await response.json();
   let visitorNumber = data.value;
-  visitorCounter.createTextNode(text);
-  text.value = visitorNumber;
+  visitorCounter.innerText = `Sivulla vierailtu ${visitorNumber} kertaa`;
 }
 
 newVisitor(hitUrl, visitorCounter)
