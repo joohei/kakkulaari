1fe41ca80ac1a2a5ea61e15ba1637e9722bc1598
joohei <joona.heimonen@icloud.com>
2022-02-22 12:43:16 +0200

    Add files via upload

--- scripts/visitor.js
+++ scripts/visitor.js
@@ -3,9 +3,10 @@ const visitorCounter = document.querySelector(".visitor-counter")
 
 async function newVisitor(hitUrl, visitorCounter) {
   let response = await fetch(hitUrl);
-  let data = await response.json()
-  let visitorNumber = data.value
-  console.log(visitorNumber)
+  let data = await response.json();
+  let visitorNumber = data.value;
+  visitorCounter.createTextNode(text);
+  text.value = visitorNumber;
 }
 
 newVisitor(hitUrl, visitorCounter)
